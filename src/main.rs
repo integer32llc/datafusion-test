60e7d900bf4a0b13e8bc6e9c993d076ef579de9f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-02-10 15:35:37 -0500

    read files into in-mem object store

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use datafusion::common::Result;
+use object_store::ObjectStore;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let data_dir = "/Users/carolnichols/Downloads/smaller-repro/";
+    let store =
+        Arc::new(object_store::memory::InMemory::new()) as Arc<dyn ObjectStore>;
+
+    println!("Reading files into memory");
+    for file in std::fs::read_dir(data_dir)? {
+        let file = file?.path();
+        println!("file: {}", file.display());
+
+        let bytes = std::fs::read(&file)?;
+
+        let path = object_store::path::Path::from(file.display().to_string());
+        let payload = object_store::PutPayload::from_bytes(bytes.into());
+        store
+            .put_opts(&path, payload, object_store::PutOptions::default())
+            .await
+            .unwrap();
+
+    }
+
+    Ok(())
 }
