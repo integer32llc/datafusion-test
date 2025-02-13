c2ccfd8071de67e7a7d3110392d34fc1fbc5d5aa
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-02-13 11:48:00 -0500

    tokio spawn with cancellation token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code, unused_variables)]
+
 use datafusion::{
     common::Result,
     datasource::{
@@ -11,7 +13,10 @@ use datafusion::{
 use futures_util::TryStreamExt;
 use log::*;
 use object_store::ObjectStore;
-use std::sync::Arc;
+use std::{sync::Arc, time::{Duration, Instant}};
+use tokio_util::sync::CancellationToken;
+
+const WAIT_BEFORE_CANCEL: Duration = Duration::from_millis(1_000);
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -33,6 +38,63 @@ async fn main() -> Result<()> {
     }
     debug!("Done loading data into in-memory object store");
 
+    let start = Instant::now();
+    let token = CancellationToken::new();
+    let captured_token = token.clone();
+
+    let join_handle = tokio::spawn(async move {
+        debug!("Starting spawned");
+        loop {
+            let store = Arc::clone(&store);
+            tokio::select! {
+                biased;
+                _ = async move {
+                    // If I use blocking sleep here, the program never exits
+                    // blocks_too_much().await;
+
+                    // If I use nonblocking sleep here, the program exits in the expected time
+                    // yields_enough().await;
+
+                    // If I run a datafusion query....
+                    datafusion(store, data_dir).await.unwrap();
+                } => {
+                    debug!("matched case doing work");
+                },
+                _ = captured_token.cancelled() => {
+                    debug!("Received shutdown request");
+                    return;
+                },
+            }
+        }
+    });
+
+    debug!("in main, non-blocking sleep");
+    tokio::time::sleep(WAIT_BEFORE_CANCEL).await;
+
+    debug!("cancelling thread");
+    token.cancel();
+
+    if let Err(e) = join_handle.await {
+        debug!("Error waiting for shutdown: {e}");
+    }
+    debug!("done with main in {:?}; expected to be around {WAIT_BEFORE_CANCEL:?}", start.elapsed());
+
+    Ok(())
+}
+
+async fn blocks_too_much() {
+    debug!("blocking sleep");
+    std::thread::sleep(Duration::from_secs(5));
+    debug!("done with blocking sleep");
+}
+
+async fn yields_enough() {
+    debug!("nonblocking sleep");
+    tokio::time::sleep(Duration::from_secs(5)).await;
+    debug!("done with nonblocking sleep");
+}
+
+async fn datafusion(store: Arc<dyn ObjectStore>, data_dir: &str) -> Result<()> {
     let query = "SELECT distinct \"A\", \"B\", \"C\", \"D\", \"E\" FROM \"test_table\"";
     let file_format = ParquetFormat::default().with_enable_pruning(true);
     let listing_options = ListingOptions::new(Arc::new(file_format))
@@ -58,15 +120,11 @@ async fn main() -> Result<()> {
     debug!("Executing physical plan...");
     let partition = 0;
     let task_context = Arc::new(TaskContext::from(&ctx));
-    let stream =
-        tokio::task::spawn_blocking(move || physical_plan.execute(partition, task_context))
-            .await
-            .unwrap()
-            .unwrap();
+    let stream = physical_plan.execute(partition, task_context)
+        .unwrap();
 
     debug!("Getting results...");
     let results: Vec<_> = stream.try_collect().await?;
-
     debug!("Got {} record batches", results.len());
 
     Ok(())
