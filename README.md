7772182f7fcc7a798da3ed43e136cf686b9e364b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2025-02-14 16:38:29 -0500

    Generate data files to reproduce the issue, if necessary

--- README.md
+++ README.md
@@ -1,13 +1,123 @@
 # Datafusion cancellation problem reproducer
 
-Setup:
+This binary will:
 
-Put at least 4 of the sample Parquet files in `data/`.
+- Look in `./data/` for `.parquet` files in the format it expects; if it doesn't find any, it will
+  generate them for you (the generation will take ~minutes but will only happen if there aren't
+  files on disk, so if you leave the files around, subsequent runs of this tool won't take as long)
+- Load the on-disk files into an in-memory object store, which is necessary to reproduce the issue
+  because reading from the in-memory object store does not `await`
+- Run a number of tests from 1ms to `MAX_WAIT_TIME` ms that:
+  - Spawns a tokio task that `select!`s between running a datafusion query and waiting on a cancellation token
+  - Waits the number of milliseconds configured for this test run
+  - Cancels the token
+  - Reports how long the cancellation took in whole milliseconds
+- Prints out the results from all test runs in a nice markdown table suitable for copying to GitHub
 
-To run:
+## How to run
+
+To best see the markdown table of results, run without any `RUST_LOG` set:
+
+```
+$ cargo run
+```
+
+To see just the debug logs from this binary:
+
+```
+$ RUST_LOG=datafusion_test=debug cargo run
+```
+
+To see all debug logs including those from datafusion:
 
 ```
 $ RUST_LOG=debug cargo run
 ```
 
-In `top`, you should see the datafusion-test binary take 400% CPU for about 8 seconds.
+## Variables to adjust
+
+Near the top of the file are three constants extracted for ease of modification:
+
+```
+/// Adjust how many data files are generated
+const NUM_FILES: usize = 7;
+/// Adjust how many rows are generated in each file
+const ROWS_PER_FILE: usize = 5_000_000;
+/// Adjust the maximum time to wait and let the datafusion code run, in milliseconds. 1 through
+/// this number of milliseconds will be tested.
+const MAX_WAIT_TIME: u64 = 50;
+```
+
+Note that if you change `NUM_FILES` and/or `ROWS_PER_FILE`, you'll need to delete/move any existing
+files in the `./data/` dir so that they get regenerated using the new values.
+
+There are many other changes that could be made to this test (for example, the types of data in the
+files, the query, datafusion settings) and of course there's probably ways to more clearly
+illustrate the problem with less data. These changes are left as an exercise for the reader.
+
+## Sample results
+
+There will be some variance in the results, due to randomness in the files, randomness in task
+running, differences in host system, etc. In general, with files generated using these values:
+
+```
+NUM_FILES = 7
+ROWS_PER_FILE = 5_000_000
+MAX_WAIT_TIME = 50
+```
+
+the results show there are some points in the datafusion processing that are not awaiting very
+often so that they take significantly more than a millisecond to cancel:
+
+| Wait time (ms) | Cancel duration (ms) |
+|----------------|----------------------|
+| 1 | 27 |
+| 2 | 5 |
+| 3 | 4 |
+| 4 | 3 |
+| 5 | 2 |
+| 6 | 0 |
+| 7 | 7 |
+| 8 | 3 |
+| 9 | 0 |
+| 10 | 0 |
+| 11 | 0 |
+| 12 | 0 |
+| 13 | 0 |
+| 14 | 0 |
+| 15 | 0 |
+| 16 | 0 |
+| 17 | 0 |
+| 18 | 0 |
+| 19 | 0 |
+| 20 | 0 |
+| 21 | 0 |
+| 22 | 0 |
+| 23 | 0 |
+| 24 | 0 |
+| 25 | 0 |
+| 26 | 0 |
+| 27 | 0 |
+| 28 | 0 |
+| 29 | 0 |
+| 30 | 0 |
+| 31 | 0 |
+| 32 | 211 |
+| 33 | 0 |
+| 34 | 0 |
+| 35 | 19 |
+| 36 | 0 |
+| 37 | 0 |
+| 38 | 15 |
+| 39 | 0 |
+| 40 | 0 |
+| 41 | 0 |
+| 42 | 0 |
+| 43 | 0 |
+| 44 | 0 |
+| 45 | 0 |
+| 46 | 35 |
+| 47 | 0 |
+| 48 | 0 |
+| 49 | 0 |
+| 50 | 0 |
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,51 @@
-#![allow(dead_code, unused_variables)]
-
+use arrow::{array::Array, datatypes::DataType, record_batch::RecordBatch};
 use datafusion::{
     common::Result,
     datasource::{
         file_format::{parquet::ParquetFormat, FileFormat},
-        listing::ListingOptions,
+        listing::{ListingOptions, ListingTableUrl},
     },
     execution::{context::TaskContext, object_store::ObjectStoreUrl},
     physical_plan::{coalesce_partitions::CoalescePartitionsExec, ExecutionPlan},
     prelude::*,
+    scalar::ScalarValue,
 };
 use futures_util::TryStreamExt;
 use log::*;
 use object_store::ObjectStore;
+use parquet::arrow::{async_writer::ParquetObjectWriter, AsyncArrowWriter};
 use std::{
+    iter::repeat_with,
+    path::PathBuf,
     sync::Arc,
     time::{Duration, Instant},
 };
 use tokio_util::sync::CancellationToken;
 
+/// Adjust how many data files are generated
+const NUM_FILES: usize = 7;
+/// Adjust how many rows are generated in each file
+const ROWS_PER_FILE: usize = 5_000_000;
+/// Adjust the maximum time to wait and let the datafusion code run, in milliseconds. 1 through
+/// this number of milliseconds will be tested.
+const MAX_WAIT_TIME: u64 = 50;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let data_dir = "./data";
+    let data_dir = "data";
+    let files_on_disk = find_or_generate_files(data_dir).await?;
+
     let store = Arc::new(object_store::memory::InMemory::new()) as Arc<dyn ObjectStore>;
 
-    for file in std::fs::read_dir(&data_dir)? {
-        let file = file?.path();
-        let bytes = std::fs::read(&file)?;
-
-        let path = object_store::path::Path::from(file.display().to_string());
-        let payload = object_store::PutPayload::from_bytes(bytes.into());
-        store
-            .put_opts(&path, payload, object_store::PutOptions::default())
-            .await
-            .unwrap();
-    }
+    debug!("Starting to load data into in-memory object store");
+    load_data(Arc::clone(&store), &files_on_disk).await?;
     debug!("Done loading data into in-memory object store");
 
     println!("| Wait time (ms) | Cancel duration (ms) |");
     println!("|----------------|----------------------|");
-    for wait_time in 1..=50 {
+    for wait_time in 1..=MAX_WAIT_TIME {
         let cancel_duration = run_test(wait_time, Arc::clone(&store), data_dir).await?;
         println!("| {wait_time} | {} |", cancel_duration.as_millis());
     }
@@ -100,12 +104,14 @@ async fn run_test(
     Ok(elapsed)
 }
 
+#[allow(dead_code)]
 async fn blocks_too_much() {
     debug!("blocking sleep");
     std::thread::sleep(Duration::from_secs(5));
     debug!("done with blocking sleep");
 }
 
+#[allow(dead_code)]
 async fn yields_enough() {
     debug!("nonblocking sleep");
     tokio::time::sleep(Duration::from_secs(5)).await;
@@ -114,18 +120,28 @@ async fn yields_enough() {
 
 async fn datafusion(store: Arc<dyn ObjectStore>, data_dir: &'static str) -> Result<()> {
     let query = "SELECT distinct \"A\", \"B\", \"C\", \"D\", \"E\" FROM \"test_table\"";
-    let file_format = ParquetFormat::default().with_enable_pruning(true);
-    let listing_options = ListingOptions::new(Arc::new(file_format))
-        .with_file_extension(ParquetFormat::default().get_ext());
 
     let config = SessionConfig::new()
         .with_target_partitions(4)
         .set_bool("datafusion.execution.parquet.pushdown_filters", true);
     let ctx = SessionContext::new_with_config(config);
     let object_store_url = ObjectStoreUrl::parse("test:///").unwrap();
-    ctx.register_object_store(object_store_url.as_ref(), store);
-    ctx.register_listing_table("test_table", data_dir, listing_options.clone(), None, None)
-        .await?;
+    ctx.register_object_store(object_store_url.as_ref(), Arc::clone(&store));
+
+    let file_format = ParquetFormat::default().with_enable_pruning(true);
+    let listing_options = ListingOptions::new(Arc::new(file_format))
+        .with_file_extension(ParquetFormat::default().get_ext());
+
+    let table_path = ListingTableUrl::parse(&format!("test:///{data_dir}/"))?;
+
+    ctx.register_listing_table(
+        "test_table",
+        &table_path,
+        listing_options.clone(),
+        None,
+        None,
+    )
+    .await?;
 
     debug!("Creating logical plan...");
     let logical_plan = ctx.state().create_logical_plan(query).await?;
@@ -145,4 +161,116 @@ async fn datafusion(store: Arc<dyn ObjectStore>, data_dir: &'static str) -> Resu
     debug!("Got {} record batches", results.len());
 
     Ok(())
+}
+
+async fn find_or_generate_files(data_dir: &'static str) -> Result<Vec<PathBuf>> {
+    let files_on_disk = find_files_on_disk(data_dir)?;
+
+    if files_on_disk.is_empty() {
+        println!("No data files found, generating (this will take a bit)");
+        generate_data(data_dir).await?;
+        println!("Done generating files");
+        let files_on_disk = find_files_on_disk(data_dir)?;
+
+        if files_on_disk.is_empty() {
+            panic!("Tried to generate data files but there are still no files on disk");
+        } else {
+            println!("Using {} files now on disk", files_on_disk.len());
+            Ok(files_on_disk)
+        }
+    } else {
+        println!("Using {} files found on disk", files_on_disk.len());
+        Ok(files_on_disk)
+    }
+}
+
+fn find_files_on_disk(data_dir: &'static str) -> Result<Vec<PathBuf>> {
+    Ok(std::fs::read_dir(&data_dir)?
+        .into_iter()
+        .filter_map(|file| {
+            let path = file.unwrap().path();
+            if path
+                .extension()
+                .map(|ext| (ext == "parquet"))
+                .unwrap_or(false)
+            {
+                Some(path)
+            } else {
+                None
+            }
+        })
+        .collect())
+}
+
+async fn load_data(
+    store: Arc<dyn ObjectStore>,
+    files_on_disk: &[PathBuf],
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    for file in files_on_disk {
+        let bytes = std::fs::read(&file)?;
+
+        let path = object_store::path::Path::from(file.display().to_string());
+        let payload = object_store::PutPayload::from_bytes(bytes.into());
+        store
+            .put_opts(&path, payload, object_store::PutOptions::default())
+            .await?;
+    }
+
+    Ok(())
+}
+
+async fn generate_data(
+    data_dir: &'static str,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let absolute = std::env::current_dir().unwrap().join(data_dir);
+    let store = Arc::new(object_store::local::LocalFileSystem::new_with_prefix(
+        absolute,
+    )?);
+
+    let columns = [
+        ("A", DataType::Float64),
+        ("B", DataType::Float64),
+        ("C", DataType::Float64),
+        ("D", DataType::Boolean),
+        ("E", DataType::Utf8),
+        ("F", DataType::Utf8),
+        ("G", DataType::Utf8),
+        ("H", DataType::Utf8),
+        ("I", DataType::Utf8),
+        ("J", DataType::Utf8),
+        ("K", DataType::Utf8),
+    ];
+
+    for file_num in 1..=NUM_FILES {
+        println!("Generating file {file_num} of {NUM_FILES}");
+        let data = columns.iter().map(|(column_name, column_type)| {
+            let column = random_data(column_type, ROWS_PER_FILE);
+            (column_name, column)
+        });
+        let to_write = RecordBatch::try_from_iter(data).unwrap();
+        let path = object_store::path::Path::from(format!("{file_num}.parquet").as_str());
+        let object_store_writer = ParquetObjectWriter::new(Arc::clone(&store) as _, path);
+
+        let mut writer = AsyncArrowWriter::try_new(object_store_writer, to_write.schema(), None)?;
+        writer.write(&to_write).await?;
+        writer.close().await?;
+    }
+
+    Ok(())
+}
+
+fn random_data(column_type: &DataType, rows: usize) -> Arc<dyn Array> {
+    let values = (0..rows).into_iter().map(|_| random_value(column_type));
+    ScalarValue::iter_to_array(values).unwrap()
+}
+
+fn random_value(column_type: &DataType) -> ScalarValue {
+    match column_type {
+        DataType::Float64 => ScalarValue::Float64(Some(fastrand::f64())),
+        DataType::Boolean => ScalarValue::Boolean(Some(fastrand::bool())),
+        DataType::Utf8 => {
+            ScalarValue::Utf8(Some(repeat_with(fastrand::alphanumeric).take(10).collect()))
+        }
+        other => unimplemented!("No random value generation implemented for {other:?}"),
+    }
 }
